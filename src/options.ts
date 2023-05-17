a7b8e4265f42fcbe7e5f899c323865da6e0ed409
Joel Kassim Moratalla <joelkasmor16@gmail.com>
2023-05-17 13:52:05 +0200

    Fix parser export

--- src/options.ts
+++ src/options.ts
@@ -1,3 +1,16 @@
-module.exports = {
-    
+import { getPlanets } from './data/parse-data.js';
+
+async function handleStart() {
+    getPlanets
 }
+async function handleGuide() {
+
+}
+async function handleKepler() {
+
+}
+async function handleExit() {
+
+}
+
+export { handleStart, handleGuide, handleKepler, handleExit }
