6716df5c693083d79f14ad395bce3cdbed91c80f
Joel Kassim Moratalla <joelkasmor16@gmail.com>
2023-05-17 11:37:29 +0200

    Write correct filters

--- src/data/filter.ts
+++ src/data/filter.ts
@@ -1,29 +1,18 @@
-function isHabitable(planet){
-    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6
-}
-
-function temperatureInterval(planet, bottomTemp, topTemp){
-    return planet['koi_teq'] >= bottomTemp && planet['koi_teq'] <= topTemp
-}
-
-function sizeInterval(planet, bottomSize, topSize){
-    return planet['koi_prad'] >= bottomSize && planet['koi_prad'] <= topSize
-}
-
-function orbitalPeriodInterval(planet, bottomPeriod, topPeriod){
-    return planet['koi_period'] >= bottomPeriod && planet['koi_period'] <= topPeriod
-}
-
 module.exports = {
     isHabitable: (planet) => {
         return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6
     },
     includedInInterval: (property, planet, bottomLimit, topLimit) => {
-        switch (key) {
-            case value:
-                
+        switch (property) {
+            case 'temperature':
+                return planet['koi_teq'] >= bottomLimit && planet['koi_teq'] <= topLimit
                 break;
-        
+            case 'size':
+                return planet['koi_prad'] >= bottomLimit && planet['koi_prad'] <= topLimit
+                break;
+            case 'orbitalPeriod':
+                return planet['koi_period'] >= bottomLimit && planet['koi_period'] <= topLimit
+                break;
             default:
                 break;
         }
